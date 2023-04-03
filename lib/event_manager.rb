6623c707b034569f446275e5dba1502bb4ef0217
Won Rhim <wwrk22@gmail.com>
2023-04-03 10:45:08 -0400

    Add legislator name collect

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,18 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
+
+
+def collect_legislator_names(legislators)
+  legislators.map(&:name)
+end
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 
 puts "Event Manager initialized"
@@ -17,10 +26,21 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislator_names = collect_legislator_names(legislators.officials).join(", ")
+  rescue Google::Apis::ClientError
+    "Failed to find representative for given inforamation. "\
+    "Find your representative at "\
+    "www.commoncause.org/take-action/find-elected-officials"
+  end
 
-  clean_zipcode(zipcode)
-
-  puts "#{name}, #{zipcode}"
+  puts "#{name}, #{zipcode}, #{legislator_names}"
 end
 
 contents.close
